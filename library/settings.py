0cd934d44886aeef2870b8b8fac53e9d6b699d54
Harsh Kumar <harshnehra760@gmail.com>
2025-04-16 14:58:18 +0530

    added cors features

--- library/settings.py
+++ library/settings.py
@@ -65,6 +65,28 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS").split(",")
 
+# Additional CORS settings
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 ROOT_URLCONF = 'library.urls'
 
 TEMPLATES = [
